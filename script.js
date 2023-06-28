7efa71e15ddf20660582c5ecc6dae2238d282f80
devthiagocesar <devthiagocesar@gmail.com>
2023-06-28 16:25:53 -0300

    260623

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 function raffle() {
+    const min = Math.ceil(document.querySelector(".input-min").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
 
-    const min = Math.ceil (document.querySelector (".input-min").value)
-    const max = Math.floor (document.querySelector (".input-max").value)
+    if (min >= max) {
+       alert("Não pode digitar ENTRE menor que E")
+    }
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-const resul = Math.floor (Math.random() * (max - min + 1)) + min;
 
-alert (resul)
+        alert(result)
+    }
+
 
 
 }
+
+
 
